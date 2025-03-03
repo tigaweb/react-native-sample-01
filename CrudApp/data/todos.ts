2dcb62c9a5bb6afac91b497bf12a6285e3689ee3
tigaweb <nka.webtech.dev@gmail.com>
2025-03-03 09:56:19 +0900

    create todo app

---
+++ CrudApp/data/todos.ts
@@ -0,0 +1,108 @@
+export type Todo = {
+  id: number,
+  title: string,
+  completed: boolean,
+}
+
+export const data: Array<Todo> = [
+  {
+    id: 1,
+    title: "Finish reading the book",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Go grocery shopping",
+    completed: false
+  },
+  {
+    id: 3,
+    title: "Complete React Native tutorial",
+    completed: true
+  },
+  {
+    id: 4,
+    title: "Walk 5,000 steps",
+    completed: false
+  },
+  {
+    id: 5,
+    title: "Prepare for the meeting",
+    completed: true
+  },
+  {
+    id: 6,
+    title: "Write a blog post",
+    completed: false
+  },
+  {
+    id: 7,
+    title: "Practice TypeScript",
+    completed: true
+  },
+  {
+    id: 8,
+    title: "Watch a documentary",
+    completed: false
+  },
+  {
+    id: 9,
+    title: "Organize workspace",
+    completed: true
+  },
+  {
+    id: 10,
+    title: "Cook dinner",
+    completed: false
+  },
+  {
+    id: 11,
+    title: "Meditate for 10 minutes",
+    completed: true
+  },
+  {
+    id: 12,
+    title: "Plan next week’s tasks",
+    completed: false
+  },
+  {
+    id: 13,
+    title: "Update resume",
+    completed: true
+  },
+  {
+    id: 14,
+    title: "Reply to emails",
+    completed: false
+  },
+  {
+    id: 15,
+    title: "Exercise for 30 minutes",
+    completed: true
+  },
+  {
+    id: 16,
+    title: "Learn a new English word",
+    completed: false
+  },
+  {
+    id: 17,
+    title: "Call a friend",
+    completed: true
+  },
+  {
+    id: 18,
+    title: "Fix a bug in the project",
+    completed: false
+  },
+  {
+    id: 19,
+    title: "Read a technical article",
+    completed: true
+  },
+  {
+    id: 20,
+    title: "Backup important files",
+    completed: false
+  }
+];
